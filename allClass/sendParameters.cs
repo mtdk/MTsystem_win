8dfde2471e733df6367de7ff6a46e793cbb85b48
mtdk <ruixiangxie@outlook.com>
2020-12-21 12:19:20 +0800

    202012211219

---
+++ allClass/sendParameters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MTsystem_win.allClass
+{
+    class sendParameters
+    {
+        private static string matid;
+
+        public static string _Matid
+        {
+            get { return matid; }
+            set { matid = value; }
+        }
+    }
+}
