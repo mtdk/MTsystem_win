835c669636f3f73bf4f72248f8cec3f282f1271f
mtdk <ruixiangxie@outlook.com>
2021-05-11 23:24:01 +0800

    202105112323

--- allClass/connectstr.cs
+++ allClass/connectstr.cs
@@ -3,9 +3,11 @@ namespace MTsystem_win.allClass
 {
     class connectstr
     {
-        public static string CONNECTSTR = "server=192.168.1.203;uid=qinghe;pwd=123456;database=mtsystemdb;charset=utf8;";
+        //public static string CONNECTSTR = "server=192.168.1.203;uid=qinghe;pwd=123456;database=mtsystemdb;charset=utf8;";
 
-        //public static string CONNECTSTR = "server=127.0.0.1;uid=qinghe;pwd=123456;database=mtsystemdb;charset=utf8;";
-        //public static string CONNECTSTR = "server=127.0.0.1;uid=testqh;pwd=123456;database=mtsystemdb;charset=utf8;";
+        //public static string CONNECTSTR = "server=172.18.213.232;uid=testqh;pwd=xrx12345678;database=mtsystemdb;charset=utf8;";
+
+        public static string CONNECTSTR = "server=127.0.0.1;uid=testqh;pwd=123456;database=mtsystemdb;charset=utf8;";
+
     }
 }
