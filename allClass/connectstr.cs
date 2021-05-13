1e62771720e0b4d9a80b2a2513350e66438e95b3
QINGHE-XRX <ruixiangxie@hotmail.com>
2021-05-13 15:37:23 +0800

    202105131537

--- allClass/connectstr.cs
+++ allClass/connectstr.cs
@@ -7,9 +7,9 @@ namespace MTsystem_win.allClass
 {
     class connectstr
     {
-        public static string CONNECTSTR = "server=192.168.1.203;uid=qinghe;pwd=123456;database=mtsystemdb;charset=utf8;";
+        //public static string CONNECTSTR = "server=192.168.1.203;uid=qinghe;pwd=123456;database=mtsystemdb;charset=utf8;";
 
         //public static string CONNECTSTR = "server=192.168.1.123;uid=testqh;pwd=123456;database=mtsystemdb;charset=utf8;";
-        //public static string CONNECTSTR = "server=127.0.0.1;uid=testqh;pwd=123456;database=mtsystemdb;charset=utf8;";
+        public static string CONNECTSTR = "server=127.0.0.1;uid=testqh;pwd=123456;database=mtsystemdb;charset=utf8;";
     }
 }
