b826a56e4ce111bb8ae8fc4742cfd97f4fac4158
QINGHE-XRX <ruixiangxie@hotmail.com>
2021-05-13 17:16:41 +0800

    202105131716

--- Frm_product_outputReport.cs
+++ Frm_product_outputReport.cs
@@ -6,6 +6,12 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Microsoft.Reporting;
+using Microsoft.Reporting.WinForms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using MTsystem_win.allClass;
+using MTsystem_win.dsControl;
 
 namespace MTsystem_win
 {
@@ -16,10 +22,51 @@ namespace MTsystem_win
             InitializeComponent();
         }
 
+        //public string out_id;
+
+        private void outPrint()
+        {
+            string sqlstr = "SELECT * FROM mtsystemdb.product_out_view";
+            ds_productoutReportview ds = new ds_productoutReportview();
+            DataTable dt = new DataTable();
+            MySqlConnection conn = new MySqlConnection(connectstr.CONNECTSTR);
+            conn.Open();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(sqlstr, conn);
+                MySqlDataReader dr = null;
+                dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                ds.Tables["tb_productAllReport"].Load(dr);
+
+                if (ds.Tables["tb_productAllReport"].Rows.Count < 6)
+                {
+                    for (int i = 6 - (ds.Tables["tb_productAllReport"].Rows.Count); i > 0; i--)
+                    {
+                        DataRow dtr = ds.Tables["tb_productAllReport"].NewRow();
+                        ds.Tables["tb_productAllReport"].Rows.Add(dtr);
+                    }
+                }
+
+                ReportDataSource rds = new ReportDataSource();
+                product_outputallReportview.LocalReport.ReportEmbeddedResource = "MTsystem_win.printForm.product_out_OutReport.rdlc";
+                rds.Name = "ds_productoutReportview";
+                rds.Value = ds.Tables["tb_productAllReport"];
+                product_outputallReportview.LocalReport.DataSources.Add(rds);
+
+                this.product_outputallReportview.SetDisplayMode(Microsoft.Reporting.WinForms.DisplayMode.PrintLayout);
+                this.product_outputallReportview.ZoomMode = ZoomMode.PageWidth;
+                this.product_outputallReportview.RefreshReport();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("错误代码：" + ex.Number + " 错误信息：" + ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
         private void Frm_product_outputReport_Load(object sender, EventArgs e)
         {
 
-            this.product_outputallReportview.RefreshReport();
+            this.product_outputallReportview.Reset();
+            outPrint();
         }
     }
 }
--- allClass/connectstr.cs
+++ allClass/connectstr.cs
@@ -4,16 +4,11 @@ namespace MTsystem_win.allClass
     class connectstr
     {
         //public static string CONNECTSTR = "server=192.168.1.203;uid=qinghe;pwd=123456;database=mtsystemdb;charset=utf8;";
-<<<<<<< HEAD
 
         //public static string CONNECTSTR = "server=192.168.1.123;uid=testqh;pwd=123456;database=mtsystemdb;charset=utf8;";
         public static string CONNECTSTR = "server=127.0.0.1;uid=testqh;pwd=123456;database=mtsystemdb;charset=utf8;";
-=======
 
         //public static string CONNECTSTR = "server=172.18.213.232;uid=testqh;pwd=xrx12345678;database=mtsystemdb;charset=utf8;";
 
-        public static string CONNECTSTR = "server=127.0.0.1;uid=testqh;pwd=123456;database=mtsystemdb;charset=utf8;";
-
->>>>>>> 835c669636f3f73bf4f72248f8cec3f282f1271f
     }
 }
